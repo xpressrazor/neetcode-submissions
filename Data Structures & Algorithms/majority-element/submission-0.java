e4002c096f350c55854f02f9498702e4f67a244d
xpressrazor <3086604+xpressrazor@users.noreply.github.com>
2026-06-05 15:39:58 -0600

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int num : nums) {
+            if (num == candidate)++count;
+            else --count;
+
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            }
+        }
+
+        return candidate;
+    }
+}
